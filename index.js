1ad42d9b6455656892a91f3c1a82fb280e1f7a7c
Dmitry16 <europartner@inbox.ru>
2018-10-23 18:35:37 +0300

    commit4

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
-// function prompt(arr, callback) {
-//     var stdin = process.stdin,
-//         stdout = process.stdout;
-//     stdin.resume();
-//     stdout.write(arr);
 
-//     stdin.once('data', function (data) {
-//         callback(data.toString().trim());
-//     });
-// }
-// prompt('lets play:', function (input) {
-//     console.log(input);
-//     process.exit();
-// });
-// const n = 5;
-// function makeArr(n) {
-//     let arr = [];
-//     for (let y=0; y<n; y++) {
-//         arr.push([]);
-//         arr[y].push( new Array(5));
-
-//         for (let x=0; x<n; x++) {
-//             arr[y][x] = 0;
-//         }
-//     }
-//     console.log(arr);
-// }
-// makeArr(5);
 let arr = [[1,0,1,0,1],[1,0,0,1,0],[0,1,1,0,1],[0,0,1,1,0],[1,1,0,1,1]];
+let mutatedArr = arr.slice();
 
 function letsPlay() {
     for (let y=0; y<arr.length; y++) {
@@ -41,44 +15,43 @@ function checkForLife(i,j) {
     }
     if (arr[i][j] === 0) {
         // checkNextDead();
-        deadCounter++;
     }
 }
 function checkNextAlive(q,w) {
-    let aliveCounter = 0;
+    let nextAlive = 0;
     
     if (arr[q][w + 1] && arr[q][w + 1] === 1) {
-        aliveCounter++;
+        nextAlive++;
     }
     if (arr[q][w - 1] && arr[q][w - 1] === 1) {
-        aliveCounter++;
+        nextAlive++;
     }
     if (arr[q + 1] && arr[q + 1][w] === 1) {
-        aliveCounter++;
+        nextAlive++;
     }
     if (arr[q - 1] && arr[q - 1][w] === 1) {
-        aliveCounter++;
+        nextAlive++;
     }
     if (arr[q + 1] && arr[q + 1][w + 1] && arr[q + 1][w + 1] === 1) {
-        aliveCounter++;
+        nextAlive++;
     }
     if (arr[q + 1] && arr[q + 1][w - 1] && arr[q + 1][w - 1] === 1) {
-        aliveCounter++;
+        nextAlive++;
     }
     if (arr[q - 1] && arr[q - 1][w + 1] && arr[q - 1][w + 1] === 1) {
-        aliveCounter++;
+        nextAlive++;
     }
     if (arr[q - 1] && arr[q - 1][w - 1] && arr[q - 1][w - 1] === 1) {
-        aliveCounter++;
+        nextAlive++;
     }
-    if (aliveCounter < 2 || aliveCounter > 3) {
-        arr[q][w] = 0;
+    if (nextAlive < 2 || nextAlive > 3) {
+        mutatedArr[q][w] = 0;
     }
 }
 
-let deadCounter = 0;
+// let deadCounter = 0;
+// let aliveCounter = 0;
 console.log(arr);
 letsPlay();
 console.log('------------------------');
-console.log(arr);
-console.log('Dead: ', deadCounter);
+console.log(mutatedArr);
